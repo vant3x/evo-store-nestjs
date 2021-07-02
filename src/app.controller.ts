5a6c3b7ff697b381fb8d02667bdb96021c6a9760
Alejandro <Velasquez>
2021-07-02 15:55:43 -0500

    creando controlador y ruta basica

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,14 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('nuevo')
+  newEndpoint() {
+    return 'soy nuevo';
+  }
+
+  @Get('/ruta/')
+  hello() {
+    return 'con slash';
+  }
 }
