2c53d9143f46e5f2f2c05230dfa4816ba964b4ca
Alejandro <Velasquez>
2021-07-02 16:30:01 -0500

    query params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -20,6 +20,13 @@ export class AppController {
     return 'con slash';
   }
 
+
+  // choque de rutas (dos rutas igual con diferente controlador) => rutas estaticas de primeras
+  @Get('products/filter')
+  getProductsFilter() {
+    return `soy un filter`;
+  }
+  
   // params forma 1
   @Get('products/:id')
   getProduct(@Param() params: any) {
@@ -29,12 +36,40 @@ export class AppController {
   // params forma 2
   @Get('product/:id')
   getProduct2(@Param('id') id: string) {
-    return `product ${id}`;
+    return `produ
+    ct ${id}`;
   }
-
   // varios params
   @Get('categories/:id/products/:productId')
   getCategory(@Param('id') id: string, @Param('productId') productId: string) {
     return `product ${productId} and category ${id}`;
   }
+
+  // query 1
+  @Get('products')
+  getProducts(@Query() params: any) {
+    const { limit, offset } = params;
+    return `products: limit => ${limit} offset => ${offset}`;
+  }
+
+  // query 2
+  @Get('products2')
+  getProducts2(
+    @Query('limit') limit: number,
+    @Query('offset') offset: number,
+    @Query('brand') brand: string,
+  ) {
+    return `products: limit => ${limit} offset => ${offset} brand => ${brand}`;
+  }
+
+  // query 3 => params por defecto (no se necesita pasar todos en la url)
+  @Get('products3')
+  getProducts3(
+    @Query('limit') limit = 20,
+    @Query('offset') offset: number,
+    @Query('brand') brand: string,
+  ) {
+    return `products: limit => ${limit} offset => ${offset} brand => ${brand}`;
+  }
+
 }
